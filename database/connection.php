bd0e959b1dc308e241af9faf3e009fc8f5280262
gabrielbarrios <dark_finder@hotmail.com>
2019-08-25 04:51:53 +0000

    database

--- database/connection.php
+++ database/connection.php
@@ -1,2 +1,20 @@
 <?php
-	echo("secret 1");
+include_once '../config/database.php';
+	
+$sql = "mysql:host=$servername;dbname=$database;";
+//$dsn_Options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
+
+$dsn_Options = [
+	PDO::ATTR_EMULATE_PREPARES   => false, // turn off emulation mode for "real" prepared statements
+	PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, //turn on errors in the form of exceptions
+	PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, //make the default fetch be an associative array
+];
+// Create a new connection to the MySQL database using PDO, $my_Db_Connection is an object
+try { 
+  $my_Db_Connection = new PDO($sql, $username, $password, $dsn_Options);
+  echo "Connected successfully";
+} catch (PDOException $error) {
+  echo 'Connection error: ' . $error->getMessage();
+}
+
+
